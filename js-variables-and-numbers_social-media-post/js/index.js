259871076b959b36cc8fe6a92c3a81658ec72a23
Predrag <predrag.babic@tactix-sports.com>
2024-06-14 11:47:08 +0200

    add js numbers file to remote

---
+++ js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,32 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+const title = "This is a title";
+console.log(title);
+const text = "This is the text";
+console.log(text);
+let numberOfLikes = 250;
+console.log(numberOfLikes);
+const userCreator = "Max Mustermann";
+console.log(userCreator);
+const isReported = true;
+console.log(isReported);
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+numberOfLikes++;
+console.log(numberOfLikes);
+// --^-- write your code here --^--
