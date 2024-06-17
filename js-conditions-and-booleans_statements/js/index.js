b338f4ebbee5a22798b87119bb0a0cc505432ccb
Predrag <predrag.babic@tactix-sports.com>
2024-06-17 11:11:24 +0200

    add solution

--- js-conditions-and-booleans_statements/js/index.js
+++ js-conditions-and-booleans_statements/js/index.js
@@ -3,24 +3,45 @@ console.clear();
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
-const receivedPassword = "password1234";
+const receivedPassword = "h4x0r1337";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
 
 // Part 2: Even / Odd
-const number = 6;
+const number = 5;
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
 
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
+const numberOfHotdogs = 1000000;
+if (numberOfHotdogs < 5) {
+  console.log("2 Euro");
+} else if (numberOfHotdogs < 100) {
+  console.log("1.50 Euro");
+} else if (numberOfHotdogs < 1000000) {
+  console.log("1 Euro");
+} else {
+  console.log("0.10 Euro");
+}
 
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 18;
 
-const statement = "";
+const statement = currentHour < 17 ? "Still need to learn.." : "Party time";
 
 console.log(statement);
 
 // Part 5: Greeting
-const userName = "Archibald";
+const userName = "Felix";
+const coach = "Felix";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (coach === userName ? "coach" : userName) + "!";
 
 console.log(greeting);
