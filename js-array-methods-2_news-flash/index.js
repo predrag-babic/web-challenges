f06ae3b04980e223546c2ac0ea78c9f688bd8b8f
Predrag <predrag.babic@tactix-sports.com>
2024-07-09 14:44:25 +0200

    solve js array methods 2 news flash challenge

---
+++ js-array-methods-2_news-flash/index.js
@@ -0,0 +1,27 @@
+import { news } from "./utils/news.js";
+import { Card } from "./components/Card/Card.js";
+import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
+
+const container = document.querySelector('[data-js="card-container"]');
+
+// Part 1 - start here
+const filteredNews = news.filter((card) => {
+  return card.categories.includes("politics");
+});
+console.log(filteredNews);
+
+// Part 2 - start here
+const sortedNews = filteredNews.toSorted((a, b) => {
+  return a.body.length - b.body.length;
+});
+console.log(sortedNews);
+
+sortedNews.forEach((news) => {
+  const cardElement = Card(news);
+  container.append(cardElement);
+});
+
+// Check your filter and sorting order here. Have a look at the console to see if you're right or wrong.
+checkFilteredNews(filteredNews);
+
+checkSortedNews(sortedNews);
