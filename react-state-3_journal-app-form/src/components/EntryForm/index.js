b4663a74e2fb33b26be8e0c4a289b80efb506858
Predrag <predrag.babic@tactix-sports.com>
2024-07-31 11:37:33 +0200

    solve react state 3 journal app form challenge

---
+++ react-state-3_journal-app-form/src/components/EntryForm/index.js
@@ -0,0 +1,33 @@
+import "./EntryForm.css";
+import Button from "../Button";
+
+export default function EntryForm({ onAddEntry }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newEntry = {
+      motto: formData.get("motto"),
+      notes: formData.get("notes"),
+    };
+    onAddEntry(newEntry);
+    event.target.reset();
+  };
+  return (
+    <form onSubmit={handleSubmit} className="entry-form">
+      <h2 className="entry-form__title">New Entry</h2>
+      <div className="entry-form__fields">
+        <div className="entry-form__field">
+          <label htmlFor="motto">Motto</label>
+          <input type="text" name="motto" id="motto" />
+        </div>
+        <div className="entry-form__field">
+          <label htmlFor="notes">Notes</label>
+          <textarea name="notes" id="notes" rows="4" />
+        </div>
+        <div className="entry-form__button-wrapper">
+          <Button type="submit">Create</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
