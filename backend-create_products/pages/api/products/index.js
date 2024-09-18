e0edb76edefb26915c6c6f8c11819364a6227f36
Predrag <predrag.babic@tactix-sports.com>
2024-09-18 10:46:21 +0200

    solve backend create products challenge

---
+++ backend-create_products/pages/api/products/index.js
@@ -0,0 +1,23 @@
+import dbConnect from "../../../db/connect";
+import Product from "../../../db/models/Product";
+
+export default async function handler(request, response) {
+  await dbConnect();
+  try {
+    if (request.method === "GET") {
+      const products = await Product.find();
+      response.status(200).json(products);
+      return;
+    }
+
+    if (request.method === "POST") {
+      const productData = request.body;
+      await Product.create(productData);
+      response.status(201).json({ message: "Product created" });
+      return;
+    }
+  } catch (error) {
+    response.status(400).json({ message: "Internal server error" });
+    return;
+  }
+}
